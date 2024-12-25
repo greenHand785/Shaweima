4e63302417f4125afb9739d2287bea8f16b93524
JESSE\90550 <905509938@qq.com>
2024-12-26 00:59:20 +0800

    【提交】：技能系统

--- Assets/Scripts/Core/Configs/Manager/JsonSheetType.cs
+++ Assets/Scripts/Core/Configs/Manager/JsonSheetType.cs
@@ -6,6 +6,6 @@ public class JsonSheetType
 {
     public static string Json_BuoyWayPointsSheet = ClientSetting.JsonSheetRoot + "/ProductLevels"; // 产品升级表
     public static string Json_LevelSheet = ClientSetting.JsonSheetRoot + "/GameLevelInfo"; // 关卡信息
-
+    public static string Json_SkillSheet = ClientSetting.JsonSheetRoot + "/SkillInfo"; // 技能信息
 }
 
